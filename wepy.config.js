a8f707b55dad94b2e111650a86eb59e7388bf186
lemon-cat-tree <1127513671@qq.com>
2026-02-28 20:06:49 +0800

    test commit

--- wepy.config.js
+++ wepy.config.js
@@ -2,16 +2,34 @@
 var prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  // 小程序编译配置
+  target: 'Weapp',
+  output: 'dist',
+  entry: 'src/app.wpy',
+  pages: [
+    'pages/load/login/index',
+    'pages/adopt-apply/manage',
+    'pages/adopt-apply/submit',
+    'pages/cat-info/detail',
+    'pages/cat-info/index',
+    'pages/dynamic/list',
+    'pages/dynamic/publish',
+    'pages/feedback/manage',
+    'pages/feedback/submit',
+    'pages/load/forget-password/index',
+    'pages/home/index',
+    'pages/mine/index',
+    'pages/load/register/index',
+    'pages/load/login/index',
+    'pages/rescue-info/list',
+    'pages/rescue-info/publish',
+    'pages/rescue-status/index',
+    'pages/volunteer-apply/manage',
+    'pages/volunteer-apply/submit'
+  ],
   wpyExt: '.wpy',
   eslint: false,
   cliLogs: !prod,
-  build: {
-    web: {
-      htmlTemplate: path.join('src', 'index.template.html'),
-      htmlOutput: path.join('web', 'index.html'),
-      jsOutput: path.join('web', 'index.js')
-    }
-  },
   resolve: {
     alias: {
       counter: path.join(__dirname, 'src/components/counter'),
